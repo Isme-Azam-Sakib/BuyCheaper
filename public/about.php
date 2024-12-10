20616fbce0fcdf65f788b0b9e150324046d25ab0
Isme Azam Sakib <ismeazamsakib@gmail.com>
2024-12-11 01:39:37 +0600

    Updated my social link.php

--- public/about.php
+++ public/about.php
@@ -300,8 +300,8 @@ include '../includes/navbar.php';
                             <p>Worked on developing the project from scratch. Also developed the primary prototype of the first increment.</p>
                         </div>
                         <div class="social-links">
-                            <a href="#"><i class="fab fa-github"></i></a>
-                            <a href="#"><i class="fab fa-linkedin"></i></a>
+                            <a href="https://github.com/Isme-Azam-Sakib"><i class="fab fa-github"></i></a>
+                            <a href="https://www.linkedin.com/in/isme-azam-sakib/"><i class="fab fa-linkedin"></i></a>
                         </div>
                     </div>
                 </div>
